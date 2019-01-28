e5b303174396367406a74afbc98d4860defa18bc
Fake Joe Budden <conbunked@gmail.com>
2019-01-28 19:59:58 +0000

    Update osbot_areas_converter.js

--- js/bot_api_converters/osbot/osbot_areas_converter.js
+++ js/bot_api_converters/osbot/osbot_areas_converter.js
@@ -61,9 +61,9 @@ export class OSBotAreasConverter extends OSBotConverter {
     
     toJavaArray(areas) {
         if (areas.areas.length === 1) {
-            return `${this.javaArea} area = ` + this.toJavaSingle(areas.areas[0]) + `;`;
+            return `public static ${this.javaArea} area = ` + this.toJavaSingle(areas.areas[0]) + `;`;
         } else if (areas.areas.length > 1) {
-            var output = `${this.javaArea}[] area = {\n`;
+            var output = `public static ${this.javaArea}[] area = {\n`;
             for (var i = 0; i < areas.areas.length; i++) {
                 output += "    " + this.toJavaSingle(areas.areas[i]);
                 if (i !== areas.areas.length - 1) {
@@ -79,9 +79,9 @@ export class OSBotAreasConverter extends OSBotConverter {
     
     toJavaList(areas) {
         if (areas.areas.length === 1) {
-            return `${this.javaArea} area = ` + this.toJavaSingle(areas.areas[0]) + ";";
+            return `public static ${this.javaArea} area = ` + this.toJavaSingle(areas.areas[0]) + ";";
         } else if (areas.areas.length > 1) {
-            var output = `List&lt;${this.javaArea}&gt; area = new ArrayList<>();\n`;
+            var output = `public static List&lt;${this.javaArea}&gt; area = new ArrayList<>();\n`;
             for (var i = 0; i < areas.areas.length; i++) {
                 output += "area.add(" + this.toJavaSingle(areas.areas[i]) + ");\n";
             }
@@ -92,7 +92,7 @@ export class OSBotAreasConverter extends OSBotConverter {
     
     toJavaArraysAsList(areas) {
         if (areas.areas.length === 1) {
-            return `${this.javaArea} area = ` + this.toJavaSingle(areas.areas[0]) + ";";
+            return `public static ${this.javaArea} area = ` + this.toJavaSingle(areas.areas[0]) + ";";
         } else if (areas.areas.length > 1) {
             var output = `public static List&lt;${this.javaArea}&gt; area = Arrays.asList(` +
                 `    new ${this.javaArea}[]{new Tile`;
